ee93e2e9567833dfff2ece14e09f6858079f93d3
John Forte <fortejohn@gmail.com>
2019-02-13 00:46:06 -0500

    Fixed typo of missing comma

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ function Cfn (name, template) {
               StackName: name,
               Capabilities: capabilities,
               Parameters: noramlizedParams,
-              DisabledRollback: disabledRollback
+              DisabledRollback: disabledRollback,
               Tags: convertTags()
             }, templateObject))
           })
